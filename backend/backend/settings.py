b5f6b6ed72ffa38c83cabe1a1089259113243ed8
Dordzhi Opiev <opiev.dordzhi@yandex.ru>
2023-09-21 13:25:15 +0300

    taski-docker

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,13 +1,17 @@
 import os
 from pathlib import Path
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('SECRET_KEY', '1234')
+SECRET_KEY = os.getenv('SECRET_KEY', 'secret_key')
 
 DEBUG = False
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost').split(",")
+ALLOWED_HOSTS = []
 
 
 # Application definition
