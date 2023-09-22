f73526daf2aadad4b5812af77f8c94b1fc305a2e
Dordzhi Opiev <opiev.dordzhi@yandex.ru>
2023-09-22 15:33:16 +0300

    taski-docker

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'secret_key')
 
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', default='127.0.0.1, localhost').strip().split(', ')
 
 
 # Application definition
